528ab0d59eb0dc933c860f5f89d475942e074475
MuriloZR <murilodojogo@gmail.com>
2025-10-09 14:08:20 -0300

    class and functions base form

---
+++ seg_tree.cpp
@@ -0,0 +1,41 @@
+
+#include <vector>
+
+template<typename T>
+class seg_tree
+{
+private:
+    int size;
+    std::vector<T> tree;  // Vetor de tipo genérico T
+public:
+    seg_tree(const std::vector<T>& arr);
+    ~seg_tree();
+    
+    void update(int pos, T value);
+    T query(int left, int right);
+};
+
+template<typename T>
+seg_tree<T>::seg_tree(const std::vector<T>& arr) : size(arr.size())
+{
+    tree.resize(4 * size);
+    // Construir a árvore aqui
+}
+
+template<typename T>
+seg_tree<T>::~seg_tree()
+{
+    // Destrutor (vector se limpa automaticamente)
+}
+
+template <typename T>
+void seg_tree<T>::update(int pos, T value)
+{
+    //atualizar a árvore aqui
+}
+
+template <typename T>
+T seg_tree<T>::query(int left, int right)
+{
+    return T(); //retorna o dado bescado entre left e right
+}
