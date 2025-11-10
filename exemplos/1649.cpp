5bbcff485f9eea1df3eaa11355de624204144eb2
Leonardo Groos Lago <leonardog_rs@yahoo.com.br>
2025-11-10 13:28:25 -0300

    Adiciona exemplos

---
+++ exemplos/1649.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "segtree.hpp"
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n, q;
+  std::cin >> n >> q;
+  std::vector<ull> v(n);
+  for(auto& i : v) std::cin >> i;
+  segTree<ull>tree(v, MIN);
+
+  for (int i = 0; i < q; i++) {
+    int op, l, r;
+    std::cin >> op >> l >> r;
+    if (op == 1) tree.update(l-1, r);
+    else std::cout << tree.query(l-1, r-1) << '\n'; 
+  }
+}
---
+++ exemplos/1650.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include "segtree.hpp"
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n, q;
+  std::cin >> n >> q;
+  std::vector<ull> v(n);
+  for(auto& i : v) std::cin >> i;
+  segTree<ull>tree(v, XOR);
+
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    std::cin >> l >> r;
+    std::cout << tree.query(l-1, r-1) << '\n'; 
+  }
+}
